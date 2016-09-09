8c129e5c3a818e6267523824fc40de3378a77dcc
andrewdanylyak <andrewdanylyak@gmail.com>
2016-09-09 15:28:16 +0300

    git_test3

---
+++ app/src/main/java/com/andriidanyliak/ble_tester/BleTools.java
@@ -0,0 +1,24 @@
+package com.andriidanyliak.ble_tester;
+
+import android.content.Context;
+import android.os.Handler;
+
+/**
+ * Created by Andrii Danyliak on 09.09.2016.
+ */
+public class BleTools {
+    private Context mMianActivityContext;
+    private Handler mMainActivityHandler;
+
+    public final class CBleEvents{
+        public static final int Created = 1;
+    }
+
+    BleTools(Context context, Handler handler){
+        mMianActivityContext = context;
+        mMainActivityHandler = handler;
+        mMainActivityHandler.obtainMessage(CBleEvents.Created,"Created").sendToTarget();
+    }
+
+
+}
--- app/src/main/java/com/andriidanyliak/ble_tester/MainActivity.java
+++ app/src/main/java/com/andriidanyliak/ble_tester/MainActivity.java
@@ -1,20 +1,30 @@
 package com.andriidanyliak.ble_tester;
 
+import android.app.Application;
+import android.os.Handler;
+import android.os.Message;
 import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BleTools bleTools = new BleTools(this, mBleToolsHandler);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
         Process.killProcess(Process.myPid());
     }
 
@@ -27,4 +37,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
     }
+
+
+
+    private Handler mBleToolsHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch(msg.what){
+                case BleTools.CBleEvents.Created:
+                    break;
+
+            }
+        }
+    };
 }
